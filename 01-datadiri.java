8be5ea6f9cbb66d379cd25685255604fb430ccb2
Lanyta Setyani Gunawan <94893138+MeiLing19@users.noreply.github.com>
2022-02-16 07:11:04 +0700

    Create 01-datadiri.java

---
+++ 01-datadiri.java
@@ -0,0 +1,14 @@
+package lany; //deklarasi package(folder yang berisi sekumpulan program java
+
+public class Lany { //blok kelas yang dapat diisi dengan method, fungsi atau variabel
+
+    public static void main(String[] args) { 
+//method atau fungsi main(blok program yg dieksekusi pertama kali)
+//main punya parameter args yg menyimpan nilai dari argumen di command line
+        
+        System.out.println("Hallo, aku adalah Lany"); //statement dan ekspresi
+        System.out.println("Aku tinggal di Madiun");
+        System.out.println("Usia ku 20 tahun");
+    }
+    
+}

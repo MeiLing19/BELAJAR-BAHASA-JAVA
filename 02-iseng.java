f673a6fa133998069d4b22ea83a3643e7f4ce2a7
Lanyta Setyani Gunawan <94893138+MeiLing19@users.noreply.github.com>
2022-02-17 21:48:27 +0700

    Create 02-iseng.java

---
+++ 02-iseng.java
@@ -0,0 +1,21 @@
+package lany;
+class LatihanAja {
+    public static void main(String args[]){
+        int var_x, var_y, var_z, r, pi;
+        double luas;
+        
+        r = 10;
+        pi = 20;
+        
+        luas = pi * r * r;
+        
+        System.out.println("Luas Lingkaran = " + luas);
+        
+        System.out.println("Hello World");
+        System.out.println("Apa kabar?");
+        var_x = 3;
+        var_y = 8;
+        var_z = var_x + var_y;
+        System.out.println("Varian Z = " + var_z);
+    }
+}
